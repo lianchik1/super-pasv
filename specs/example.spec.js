7e50669a4c3f1d9e1dccfa65106b84896c781f0c
lianchik1 <98603241+lianchik1@users.noreply.github.com>
2022-02-06 23:42:51 -0500

    add auth tests (#3)

--- specs/example.spec.js
+++ specs/example.spec.js
@@ -6,11 +6,11 @@ describe('Math functions',function () {
     const c = 9
     const d = -1
 
-  it.only('A + B = C', function () {
+  it.skip('A + B = C', function () {
     expect(a + b).to.eq(c)
   })
 
-  it.skip('A - B = D', function () {
+  it.only('A - B = D', function () {
     expect(a - b).to.eq(d)
     
   })
